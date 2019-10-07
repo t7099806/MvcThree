d31dab0e6703575ce8934c95147f3c5b0b42cd24
JACK FERGUSON <T7099806@live.tees.ac.uk>
2019-10-07 13:24:56 +0100

    created mvc controller

---
+++ Data/MvcMovieThreeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovieThree.Models
+{
+    public class MvcMovieThreeContext : DbContext
+    {
+        public MvcMovieThreeContext (DbContextOptions<MvcMovieThreeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieThree.Models;
 
 namespace MvcMovieThree
 {
@@ -33,6 +35,9 @@ namespace MvcMovieThree
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MvcMovieThreeContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieThreeContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
