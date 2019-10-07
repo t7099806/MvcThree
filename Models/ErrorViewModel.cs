9005708ddf892ce31512133e769af7105ff0eba2
JACK FERGUSON <T7099806@live.tees.ac.uk>
2019-10-07 13:22:28 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcMovieThree.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
